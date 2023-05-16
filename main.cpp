bc7efa0dd6a5dbba9f8a8140e7b6d3d45a856443
Rewdawrath <132688148+Rewdawrath@users.noreply.github.com>
2023-05-16 08:41:43 +0000

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
+#define SIZE 10
 #include <iostream>
+#include <iomanip>
+#include <string.h>
 using namespace std;
 
 int main(){
 
-  int a;
+  int *pa, *pb, i, temp;
+  int a[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  10 };
+  pa = &a[0];
+  cout<< "Original: ";
+  for (i = 0; i < SIZE - 1; i++,pa++){
+    cout<<setw(3)<<*pa;
+  }
+  cout<<setw(3)<<*pa<<endl;
+
+  pa = &a[0];
+  pb = &a[SIZE - 1];
+  cout<<"Reverse: ";
+  for (i = 0; i < SIZE / 2; i++) {
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+    pa++; 
+    pb--;
+  }
+  pa = &a[0];
+  for (i = 0; i < SIZE - 1; i++,pa++){
+    cout<<*pa<<" ";
+    
+  }
+  cout<<*pa<<endl;
+  return 0;
+}
+
+  
+  
+  /*int a;
   double d;
   cout<<&a<<" size "<<sizeof(a) <<endl;
   cout<<&d<<" size "<<sizeof(d) <<endl; 
-  
+  */
   /*  int x,*p;
     p=&x;
     x=5;
@@ -17,7 +51,7 @@ int main(){
     cout<< &p <<endl;*/
 
 /*Example Array*/
-  /*
+ /*
 int *ip;
 int a[ ] = {31415, 21828};
 ip = a;
@@ -37,4 +71,4 @@ cout<<ip<<" "<<*ip<<endl; 	// print
  /*the address is 4324, not 4321
  because the pointer is incremented 						
  by the size of an integer (4 bytes) */
-}
+
